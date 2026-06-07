34c791971b70ccf4b79d3faf1fde160fcd6ae14e
wzz <1761365378@qq.com>
2026-06-07 09:17:57 +0800

    修bug

--- src/main/java/net/wzz/more_avaritia/mixin/MixinLivingEntity.java
+++ src/main/java/net/wzz/more_avaritia/mixin/MixinLivingEntity.java
@@ -1,7 +1,6 @@
 package net.wzz.more_avaritia.mixin;
 
 import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -9,7 +8,6 @@ import net.minecraft.world.entity.player.Player;
 import net.wzz.more_avaritia.util.InfinityUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -36,15 +34,12 @@ public abstract class MixinLivingEntity {
         }
     }
 
-    /**
-     * @author wzz
-     * @reason use
-     */
-    @Overwrite
-    public void setHealth(float p_21154_) {
-        if (livingEntity instanceof Player player && InfinityUtils.hasInfinityArmor(player))
+    @Inject(method = "setHealth", at = @At("HEAD"), cancellable = true)
+    private void onSetHealth(float p_21154_, CallbackInfo ci) {
+        if (livingEntity instanceof Player player && InfinityUtils.hasInfinityArmor(player)) {
             livingEntity.entityData.set(DATA_HEALTH_ID, this.getMaxHealth());
-        livingEntity.entityData.set(DATA_HEALTH_ID, Mth.clamp(p_21154_, 0.0F, this.getMaxHealth()));
+            ci.cancel();
+        }
     }
 
     @Inject(method = "knockback", at = @At("HEAD"), cancellable = true)
